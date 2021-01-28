be7a698f2af0363b5d13ac5798df532b6ffaa62e
JackPawsey <jfrewer@live.co.uk>
2021-01-28 19:52:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 module "calm" {
-  source = "git@gitlab.com:simply-calm/terraform-module.git?ref=v1.0"
+  source = "git@gitlab.com:simply-calm/terraform-module.git?ref=v1.1"
   
   name = "calm"
 
